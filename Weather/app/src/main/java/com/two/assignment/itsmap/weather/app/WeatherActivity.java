93c1a47745b9fb6090cbdd9389542fb4ada01fec
jacobboes <jacobboeseriksen@hotmail.com>
2017-05-08 11:06:59 +0200

    IWeatherService

--- Weather/app/src/main/java/com/two/assignment/itsmap/weather/app/WeatherActivity.java
+++ Weather/app/src/main/java/com/two/assignment/itsmap/weather/app/WeatherActivity.java
@@ -1,9 +1,11 @@
 package com.two.assignment.itsmap.weather.app;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.two.assignment.itsmap.weather.R;
+import com.two.assignment.itsmap.weather.service.WeatherService;
 
 public class WeatherActivity extends AppCompatActivity {
 
@@ -11,5 +13,6 @@ public class WeatherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+        startService(new Intent(WeatherActivity.this, WeatherService.class));
     }
 }
---
+++ Weather/app/src/main/java/com/two/assignment/itsmap/weather/model/WeatherInfo.java
@@ -0,0 +1,10 @@
+package com.two.assignment.itsmap.weather.model;
+
+import java.util.Date;
+
+public class WeatherInfo {
+    public long id;
+    public String description;
+    public double temp;
+    public Date date;
+}
